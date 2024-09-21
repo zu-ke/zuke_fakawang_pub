2586213e13878703d75bac71b4ab0bdbf4dbcc67
zuke <zukedog@163.com>
2024-09-21 14:12:16 +0800

    1.调整蓝图支付回调方法，提高稳定性。 2.优化邮件发送功能。 3.优化前端支付逻辑。

--- src/main/java/com/zk/controller/UserController.java
+++ src/main/java/com/zk/controller/UserController.java
@@ -16,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
